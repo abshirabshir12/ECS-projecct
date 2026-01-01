963ab449b88ec7566a4e421435aa1e605692e1bf
abshirabshir12 <abshirabshir0321@gmail.com>
2026-01-01 19:41:07 +0000

    working on github actionsv13

--- infra/variables.tf
+++ infra/variables.tf
@@ -14,10 +14,6 @@ variable "aws_region" {
    default = "10.0.0.0/16"
  }
 
-variable "cidr_block" {
-  type = string
-  default = "10.0.0.0/16"
-}
 
 variable "public_subnet_cidrs" {
   type = list(string)
