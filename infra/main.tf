e59a7b96210356e29bce9d89634d7f535db886e2
abshirabshir12 <abshirabshir0321@gmail.com>
2026-01-08 14:11:15 +0000

    working on github actionsv27

--- infra/main.tf
+++ infra/main.tf
@@ -6,7 +6,7 @@ terraform {
     key            = "infrastructure/terraform.tfstate"
     region         = "eu-west-2"
     profile        = "default"
-    dynamodb_table = "terraform-state-lock"
+    use_lockfile = true
     encrypt        = true
   }
 }
